f6182d3722025ab272f10037787863531f98bbc8
Usuario <Usuario@DESKTOP-EPC6TFJ>
2022-12-09 19:24:49 -0300

    Agregar archivos de proyecto.

---
+++ BlazorApp1/Shared/Models/TimesEtapa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Shared.Models
+{
+    public class TimesEtapa
+    {
+        public string Etapa { get; set; }
+        public List<(string accion , DateTime time)> Times { get; set; }
+        public int? TimeTotalEtapa { get; set; }
+
+        public TimesEtapa (string etapa)
+        {
+            this.Etapa = etapa;
+            this.Times = new();
+            Times.Add(("Entrada", DateTime.Now));
+        }
+    }
+}
